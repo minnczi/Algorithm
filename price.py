45da2ac80292bc2327f4a5d9f43d7fbba6eea64a
kmj1995kr <32619843+kmj1995kr@users.noreply.github.com>
2020-08-20 21:55:02 +0900

    price.py modified

--- price.py
+++ price.py
@@ -1,7 +1,18 @@
-answer = []
+def solution_2(prices):
+    answer = [0] * len(prices)
+    answer[-2] = 1
+    for i in len(prices)-2:
+        seconds = 0
+        for future_price in prices:
+            if prices[i] > future_price:
+                break
+            else:
+                seconds += 1
+        answer[i] = seconds
+    return answer
 
 
-def solution(prices):
+def solution_1(prices):
     answer = []
     for i in range(len(prices)):
         moment_price = prices[i]
@@ -23,20 +34,21 @@ def solution(prices):
         answer.append(seconds)
     return answer
 
-# answer = []
-# def solution(prices):
-#     if len(prices) == 2:
-#         answer.append(1)
-#         answer.append(0)
-#         return answer
-#     else:
-#         seconds = 0
-#         for i in range (1,len(prices)):
-#             if prices[i] >= prices[0]:
-#                 seconds += 1
-#             else:
-#                 answer.append(seconds+1)
-#                 return solution(prices[1:])
-#         answer.append(seconds)
-#         return solution(prices[1:])
+answer = []
+
+def solution(prices):
+    if len(prices) == 2:
+        answer.append(1)
+        answer.append(0)
+        return answer
+    else:
+        seconds = 0
+        for i in range (1,len(prices)):
+            if prices[i] >= prices[0]:
+                seconds += 1
+            else:
+                answer.append(seconds+1)
+                return solution(prices[1:])
+        answer.append(seconds)
+        return solution(prices[1:])
 
