ca72ee692eb5e359966b9d5027e7d58b6d7c312d
kmj1995kr <32619843+kmj1995kr@users.noreply.github.com>
2020-08-21 21:21:00 +0900

    network.py solution try 1

--- network.py
+++ network.py
@@ -1,2 +1,22 @@
-list = [i for i in range (1,10)]
-print(list)
+def connection(idx, n, computers, connection_list):
+    for j in range(idx, n):
+        if j not in connection_list:
+            print(j)
+            pass
+        elif computers[idx][j] == 1:
+            print(idx, j)
+            connection_list.pop(connection_list.index(idx))
+            idx = j
+            print(idx)
+            return connection(idx, n, computers, connection_list)
+
+
+def solution(n, computers):
+    answer = 0
+    connection_list = list(range(0, n))
+    for i in range(0, n):
+        computers[i][i] = 0
+
+    connection(0, n, computers, connection_list)
+    answer = len(connection_list)
+    return answer
