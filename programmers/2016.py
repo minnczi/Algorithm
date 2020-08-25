99f8fdcd40a5066a9ca2b0ea12b14505eb206acd
kmj1995kr <32619843+kmj1995kr@users.noreply.github.com>
2020-08-25 22:35:00 +0900

    2016

---
+++ programmers/2016.py
@@ -0,0 +1,9 @@
+def solution(a, b):
+    #달별로 한달에 몇일이 있는지 저장하는 리스트
+    days_in_a_month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    
+    nth_day = sum(days_in_a_month[0:a-1]) + b
+
+    day_of_week = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"]
+    print(day_of_week[nth_day % 7])
+    return day_of_week[nth_day % 7]
