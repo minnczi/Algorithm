76b4ed1f855292a56ebd25affd4c3972205f2ec4
kmj1995kr <32619843+kmj1995kr@users.noreply.github.com>
2020-09-27 00:17:42 +0900

    phone numbers solution added

---
+++ programmers/phone_numbers.py
@@ -0,0 +1,45 @@
+#내 풀이
+def solution(phone_book):
+    for i in range(0, len(phone_book)-1):
+        for j in range(i+1, len(phone_book)):
+            if len(phone_book[i]) <= len(phone_book[j]):
+                short, long = phone_book[i], phone_book[j]
+            else:
+                short, long = phone_book[j], phone_book[i]
+
+            if short == long[0:len(short)]:
+                return False
+    return True
+
+#해쉬 사용한 풀이
+def solution(phone_book):
+    answer = True
+    hash_map = {}
+    for phone_number in phone_book:
+        hash_map[phone_number] = 1
+    for phone_number in phone_book:
+        temp = ""
+        for number in phone_number:
+            temp += number
+            if temp in hash_map and temp != phone_number:
+                answer = False
+    return answer
+
+#다른 깔끔한 풀이
+def solution(phoneBook):
+    phoneBook = sorted(phoneBook)
+
+    for p1, p2 in zip(phoneBook, phoneBook[1:]):
+        if p2.startswith(p1):
+            return False
+    return True
+
+
+from itertools import combinations as c
+def solution(phoneBook):
+    answer = True
+    sortedPB = sorted(phoneBook, key= len)
+    for (a,b) in c( sortedPB,2):
+        if a == b[:len(a)]:
+            answer = False
+    return answer
--- programmers/word_change.py
+++ programmers/word_change.py
@@ -1,50 +1,55 @@
-def similar_words(word1, word2):
-    diff = 0
-    for i in range (0, len(word1)):
-        if word1[i] != word2[i]:
-            diff += 1
-    if diff == 1:
-        return True
-    else:
-        return False
-               
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+answer = 0
 def solution(begin, target, words):
-    print(similar_words("hat", "bat"))
-    print(similar_words("hat", "cap"))
-    print(begin[0])
-    print(len(begin))
 
-# visited_list = []
-# answer = 0
+    dfs(begin, target, 0, words)
+    return answer
 
-# def solution(begin, target, words):
-#     global answer
-#     queue = [begin]
-#     #word list에 있고 아직 방문하지 않은 단어중에서
-#     while queue:
-#         current_word = queue.pop(0)
-#         visited_list.append(current_word)
-#         print("current word:", current_word)
-#         #만약 word=target이면 바로 답 반환
-#         if current_word == target:
-#             return answer
-#         #begin과 word가 정확히 1글자 차이나는지 검증
-#         else:
-#             difference = 0
-#             for word in words:
-#                 print(word)
-#                 if word in visited_list:
-#                     continue
-#                 else:
-#                     for i in range(0, len(current_word)):
-#                         #만약 두글자 이상 다를 경우 불가능이므로 break
-#                         if difference >= 2:
-#                             break
-#                         #한글자가 다를때마다 += 1
-#                         elif word[i] != begin[i]:
-#                             difference += 1
-#                         #정확히 한글자 차이나는 단어들을 queue에 넣기
-#                         if difference == 1:
-#                             queue.append(word)
-#         answer += 1
-#     return answer
+def change(fr, to):
+    for i in range(len(fr)):
+        if fr[:i]+fr[i+1:] == to[:i]+to[i+1:]:
+            return True
+    return False
+
+def dfs(begin, target, d, words):
+    global answer
+    # print(begin)
+    # print(words)
+    if begin == target:
+        # print(d)
+        answer = d
+        return
+    else:
+        if len(words) == 0:
+            return 
+        for w in range(len(words)):
+            if change(begin, words[w]):
+                word = words[:w]+words[w+1:]
+                dfs(words[w], target, d+1, word)
