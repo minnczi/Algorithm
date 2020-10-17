8e3b2e4380c1fecfc5dfd798831bd9513153e9fa
kmj1995kr <32619843+kmj1995kr@users.noreply.github.com>
2020-10-17 23:42:17 +0900

    h-index modified

--- programmers/h-index.py
+++ programmers/h-index.py
@@ -2,6 +2,7 @@ def solution(citations):
     # up = h번 이상 인용된 논문
     up = 1
     citations.sort(reverse=True)
+    # 세가지 경우의 수 반영
     for i in citations:
         if up == i:
             return i
