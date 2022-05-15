3303b15dff5bc78150bf2dbca0e77c919c985f86
Minji <kmj1995kr@gmail.com>
2022-05-16 07:45:07 +0900

    SOLVED - BOJ_6137

--- BOJ/BOJ_6137.py
+++ BOJ/BOJ_6137.py
@@ -1,36 +1,42 @@
 # BOJ_6137 문자열 생성
+"""
+f = front
+b = back
+tf = temporary front
+tb = temporary back
+N = 문자 갯수
+S = 문자열 배열 (list)
+T = 새로 만드는 문자열 (string)
+"""
 
 N = int(input())
-letters = [input() for _ in range(N)]
+S = [input() for _ in range(N)]
 T = ''
-F, B = 0, N-1
-stack = []
-temp = 0
+f, tf, b, tb = 0, 0, N-1, N-1
 
-while True:
-    if B <= F:
-        if not stack:
-            break
-        else:
-            continue
-
-    if letters[F] == letters[B]:
-        stack.append(letters[F])
-        F, B = F+1, B-1
-        continue
-    
-    
-    while stack:
-        temp = len(stack)
-        T += stack.pop(0)
-    
-    if letters[F] <= letters[B]:
-        T += letters[F]
-        F, B = F+1, B+temp
+while len(T) < N:
+    # 가장 가운데까지 비교했는데도 사전 순서가 동일하다면
+    # 예) abcba -> c까지 비교했는데 순서가 전부 동일
+    # -> 앞 문자열을 추가하고 다음으로 넘어간다
+    if tb <= tf:
+        T += S[f]
+        f += 1
+    # f가 사전순으로 앞에 있으면
+    elif S[tf] < S[tb]:
+        T += S[f]
+        f += 1
+    # b가 사전순으로 앞에 있으면
+    elif S[tb] < S[tf]:
+        T += S[b]
+        b -= 1
+    # f == b라면
     else:
-        T += letters[B]
-        F, B = F-temp, B-1
-    temp = 0
+        tf, tb = tf+1, tb-1
+        continue
+    tf, tb = f, b
 
-T += letters[F]
-print(T)
+for i in range(len(T)):
+    print(T[i], end="")
+    if i % 80 == 79:
+        print("")
+        
