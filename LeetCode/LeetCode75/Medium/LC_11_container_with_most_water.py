27e7f7a69f1012414bbde0d6fde690bad55bdd00
minnczi <kmj1995kr@gmail.com>
2023-08-27 16:44:16 +0900

    LeetCode Container with Most Water problem solved:

---
+++ LeetCode/LeetCode75/Medium/LC_11_container_with_most_water.py
@@ -0,0 +1,26 @@
+"""
+l = left
+r = right
+tl = tmp left
+tr = tmp right
+"""
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l, tl, r, tr = 0, 0, len(height)-1, len(height)-1
+        area = (r-l) * min(height[l], height[r])
+        print(area)
+
+        while tl < tr:
+            tmp_area = (tr-tl) * min(height[tl], height[tr])
+            # 이동한 포인터 인덱스로 계산한 넓이가 더 넓을경우 l, r 값 갱신
+            if tmp_area > area:
+                l, r, area = tl, tr, tmp_area
+            # 포인터 인덱스 이동
+            if height[tl] <= height[tr]:
+                tl += 1
+            else:
+                tr -= 1
+
+        return area
