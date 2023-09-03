c054590605718889de9e57a2108b84df9456f278
minnczi <kmj1995kr@gmail.com>
2023-09-03 13:39:44 +0900

    prefix_sum 알고리즘을 사용한 풀이 추가

--- LeetCode/LeetCode75/Easy/LC_1732_find_the_highest_altitude.py
+++ LeetCode/LeetCode75/Easy/LC_1732_find_the_highest_altitude.py
@@ -6,3 +6,13 @@ class Solution:
             curr_altitude += gain[i]
             max_altitude = max(max_altitude, curr_altitude)
         return max_altitude
+
+# Solution using prefix sum list
+
+
+class Solution2:
+    def largestAltitude(self, gain: List[int]) -> int:
+        prefix_sum = [gain[0]]
+        for i in range(1, len(gain)):
+            prefix_sum.append(prefix_sum[i-1] + gain[i])
+        return max(max(prefix_sum), 0)
