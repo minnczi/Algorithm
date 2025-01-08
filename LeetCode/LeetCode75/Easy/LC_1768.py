999a1254f83d2c65e6aca2ff90c74d077ce469e1
minnczi <kmj1995kr@gmail.com>
2025-01-08 23:49:00 +0900

    LC_1768 solved

--- LeetCode/LeetCode75/Easy/LC_1768.py
+++ LeetCode/LeetCode75/Easy/LC_1768.py
@@ -1,13 +1,12 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        len1, len2 = len(word1), len(word2)
-        max_len = max(len(word1), len(word2))
-        idx = 0
-        ans = ""
-        while idx < max_len:
-            if idx < len(word1):
-                ans += word1[idx]
-            if idx < len(word2):
-                ans += word2[idx]
-            idx += 1
-        return ans
+        short_len = min(len(word1), len(word2))
+        long_len = max(len(word1), len(word2))
+
+        word = ''
+        for i in range(short_len):
+            word += word1[i] + word2[i]
+        
+        word += word1[short_len:long_len] + word2[short_len:long_len]
+        return word
+        
