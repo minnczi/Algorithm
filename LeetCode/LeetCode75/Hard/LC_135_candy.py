8f10d024a4d4ce6b7ebf12d7f7a16e3076621897
minnczi <kmj1995kr@gmail.com>
2025-02-22 22:33:20 +0900

    250222 solved

---
+++ LeetCode/LeetCode75/Hard/LC_135_candy.py
@@ -0,0 +1,15 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        if len(ratings) == 1:
+                return 1
+
+        candies = [1]*len(ratings)
+        for i in range(1, len(ratings)):
+            if ratings[i] > ratings[i-1]:
+                candies[i] = candies[i-1] + 1
+        
+        for j in range(len(ratings)-2, -1, -1):
+            if (ratings[j] > ratings[j+1]) and (candies[j] <= candies[j+1]):
+                candies[j] = candies[j+1] + 1
+        
+        return sum(candies)
